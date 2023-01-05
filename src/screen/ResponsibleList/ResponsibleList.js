a893df9f194eb3ea1f9f65ab8353ee7f1e91ae05
victor <vr24355@gmail.com>
2023-01-05 04:57:36 -0500

    Vista responsable lista posdata hipolito la perra

--- src/screen/ResponsibleList/ResponsibleList.js
+++ src/screen/ResponsibleList/ResponsibleList.js
@@ -1,104 +1,196 @@
-import React, {useEffect} from "react";
-import { View, Text, SafeAreaView, ScrollView } from 'react-native';
-import { Icon } from 'react-native-elements';
-import normalize from 'react-native-normalize';
-import RNPickerSelect from 'react-native-picker-select';
-import { Footer, HeaderPercentage } from '../../components';
-import { ButtonsQuestion } from '../../components/question';
-import CustomerHeader from '../../navigation/CustomerHeader';
-import { stylesGlobal, theme } from '../../utils';
-import { styles } from './ResponsibleList.style';
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import { Icon } from "react-native-elements";
+import normalize from "react-native-normalize";
+import RNPickerSelect from "react-native-picker-select";
+import { Footer, HeaderPercentage } from "../../components";
+import { ButtonsQuestion } from "../../components/question";
+import CustomerHeader from "../../navigation/CustomerHeader";
+import { stylesGlobal, theme } from "../../utils";
+import { styles } from "./ResponsibleList.style";
 import { useTranslation } from "react-i18next";
 
-
 export function ResponsibleList(props) {
-    const { route } = props;
-    const { t, i18n } = useTranslation();
+  const dataListResponsable = [
+    { label: "Hipólito Patiño Beltrán", value: "110238455" },
+    { label: "Camilo Cortez", value: "34455" },
+    { label: "Maria Helena Martinez", value: "33455" },
+    { label: "Victor Ruiz", value: "334552" },
+  ];
+  const { route } = props;
+  const [listResponsible, setListResponsible] = useState([]);
+  const { t, i18n } = useTranslation();
+  const [dataResponsable, setDataResponsable] = useState(dataListResponsable);
+  const [numConfort, setNumConfort] = useState(0);
 
-    const loaderLanguage = async () => {
-      const DataLenguage = await storageResult.getDataFormat("@SessionLanguage");
-      i18n.changeLanguage(DataLenguage);
+  const loaderLanguage = async () => {
+    const DataLenguage = await storageResult.getDataFormat("@SessionLanguage");
+    i18n.changeLanguage(DataLenguage);
+  };
+
+  useEffect(() => {
+    loaderLanguage();
+  }, []);
+
+  const pickerStyle = {
+    inputIOS: {
+      fontFamily: "kodchasan-extraLight",
+      color: theme.GlobalColorsApp.btnGray,
+      height: normalize(40, "height"),
+      padding: normalize(10),
+    },
+    placeholder: {
+      color: theme.GlobalColorsApp.btnGray,
+    },
+    inputAndroid: {
+      fontFamily: "kodchasan-extraLight",
+      color: theme.GlobalColorsApp.btnGray,
+      height: normalize(50, "height"),
+      paddingLeft: normalize(10),
+    },
+  };
+
+  const selectedLMAction = (value, idCategory) => {};
+
+  const selectedListAdd = (value, label) => {
+    let objData = {};
+    var propertyName = value;
+    objData = {
+      label: label,
+      value: value,
     };
-    useEffect(() => {
-      loaderLanguage();
-    }, []);
 
-    const pickerStyle = {
-        inputIOS: {
-            fontFamily: 'kodchasan-extraLight',
-            color: theme.GlobalColorsApp.btnGray,
-            height: normalize(40, 'height'),
-            padding: normalize(10)
-        },
-        placeholder: {
-            color: theme.GlobalColorsApp.btnGray,
-        },
-        inputAndroid: {
-            fontFamily: 'kodchasan-extraLight',
-            color: theme.GlobalColorsApp.btnGray,
-            height: normalize(50, 'height'),
-            paddingLeft: normalize(10)
-        },
-    };
+    setNumConfort(listResponsible.length + 1);
 
-    const selectedLMAction = (value, idCategory) => {
+    setListResponsible([...listResponsible, objData]);
+  };
+  const deleteItemList = (key) => {
+    console.log(listResponsible);
+    const newListResponsible = listResponsible.filter(
+      (item) => item.value !== key
+    );
+    setNumConfort(newListResponsible.length);
 
-    }
+    setListResponsible(newListResponsible);
+  };
 
-    const ItemResponsible = (props) => {
-        const { name } = props;
-        return (
-            <View style={styles.containerList}>
-                <Text style={styles.nameList}>{name}</Text>
-                <Icon style={styles.iconList} type="ionicon" color={theme.GlobalColorsApp.btnRed} size={normalize(25)} name="trash-outline" iconStyle={{}} />
-            </View>
-        )
-    }
+  const deleteItem = (key) => {
+    console.log(dataResponsable);
+    const newDataResponsable = dataResponsable.filter(
+      (item) => item.value !== key
+    );
+    setDataResponsable(newDataResponsable);
+  };
 
+  const ItemResponsible = (props) => {
+    const { name, value } = props;
     return (
-        <SafeAreaView style={stylesGlobal.contentGlobal}>
-            <CustomerHeader />
-            <ScrollView>
-                <HeaderPercentage
-                    idCheckList={route.params.idCheckList}
-                    idCategory={route.params.id}
-                    numberQuestion={route.params.numberQuestion + 1}
-                    insideQuestion={'1'}
-                />
-                <View style={styles.container}>
-                    <View style={styles.containerHeader}>
-                        <Text style={styles.lblTitle}>{t("ResponsableList.title")}</Text>
-                        <View style={styles.emulateStyleCombo}>
-                            <RNPickerSelect
-                                name='gravity'
-                                onValueChange={(value) => { }}
-                                useNativeAndroidPickerStyle={false}
-                                placeholder={{
-                                    label: t("ResponsableList.inputPlaceholder"),
-                                    value: null,
-                                }}
-                                dropdownIconColor='red'
-                                style={pickerStyle}
-                                items={[
-                                    { label: 'Hipólito Patiño Beltrán', value: '110238455' },
-                                    { label: 'Camilo Cortez', value: '34455' },
-                                    { label: 'Maria Helena Martinez', value: '33455' },
-                                ]}
-                            />
-                        </View>
-                    </View>
-                    <ItemResponsible name='Hipólito Patiño Beltrán' />
+      <View style={styles.containerList}>
+        <Text style={styles.nameList}>{name}</Text>
+        <Icon
+          style={styles.iconList}
+          type="ionicon"
+          color={theme.GlobalColorsApp.btnRed}
+          size={normalize(25)}
+          name="trash-outline"
+          iconStyle={{}}
+          onPress={() => {
+            deleteItemList(value);
+          }}
+        />
+      </View>
+    );
+  };
 
-                </View>
+  return (
+    <SafeAreaView style={stylesGlobal.contentGlobal}>
+      <CustomerHeader />
+      <ScrollView>
+        <HeaderPercentage
+          idCheckList={route.params.idCheckList}
+          idCategory={route.params.id}
+          numberQuestion={route.params.numberQuestion + 1}
+          insideQuestion={"1"}
+        />
+        <View style={styles.container}>
+          <View style={styles.containerHeader}>
+            <Text style={styles.lblTitle}>{t("ResponsableList.title")}</Text>
+            <View style={styles.emulateStyleCombo}>
+              <RNPickerSelect
+                name="gravity"
+                onValueChange={(value, index) => {
+                  let i = index - 1;
+                  selectedListAdd(value, dataResponsable[i].label);
+                }}
+                useNativeAndroidPickerStyle={false}
+                placeholder={{
+                  label: t("ResponsableList.inputPlaceholder"),
+                  value: null,
+                }}
+                dropdownIconColor="red"
+                style={pickerStyle}
+                items={dataResponsable}
+              />
+            </View>
+          </View>
+          {listResponsible &&
+            listResponsible.map((item, key) => {
+              //console.log("victor: " +JSON.stringify(item));
 
-                <ButtonsQuestion
-                    idCategory={route.params.id}
-                    numberQuestion={route.params.numberQuestion + 1}
-                    screenResponsileList={1}
-                    idCheckList={route.params.idCheckList}
-                />
-                <Footer />
-            </ScrollView>
-        </SafeAreaView>
-    )
+              return <ItemResponsible name={item.label} value={item.value} />;
+            })}
+        </View>
+        <View style={styles.container}>
+          <TouchableOpacity
+            style={{ ...styles.item, borderColor: "red", opacity: 0.5 }}
+            // onPress={() => managerScreen(navigation, id, module, idCheckList)}
+            // key={id}
+          >
+            <View style={{ ...styles.containerIcon }}>
+              <Icon
+                type="foundation"
+                name="dislike"
+                style={styles.iconMain}
+                color="red"
+                size={normalize(30)}
+              />
+            </View>
+            <View style={styles.containerLabels}>
+              <Text style={{ ...styles.lblCategoryName, color: "red" }}>
+                No conformidades
+              </Text>
+              <Text style={styles.lblNumQuestions}>
+                <Text style={{ color: "#F27629" }}>
+                  {numConfort}{" "}
+                  <Text style={{ color: "black" }}> ña-conformidad</Text>
+                </Text>
+              </Text>
+            </View>
+            <View style={styles.containerArrow}>
+              <Icon
+                type="ionicon"
+                name="arrow-down-circle-outline"
+                color="red"
+                size={normalize(30)}
+              />
+            </View>
+          </TouchableOpacity>
+        </View>
+
+        <ButtonsQuestion
+          idCategory={route.params.id}
+          numberQuestion={route.params.numberQuestion + 1}
+          screenResponsileList={1}
+          idCheckList={route.params.idCheckList}
+        />
+        <Footer />
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
--- src/screen/ResponsibleList/ResponsibleList.style.js
+++ src/screen/ResponsibleList/ResponsibleList.style.js
@@ -1,55 +1,116 @@
 import { Platform, StyleSheet } from "react-native";
 import normalize from "react-native-normalize";
 import { RFPercentage } from "react-native-responsive-fontsize";
-import { theme } from "../../utils";
-
-
+import { theme, stylesGlobal } from "../../utils";
 
 export const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: normalize(35),
+    marginBottom: normalize(40),
+  },
 
-    container: {
-        marginHorizontal: normalize(35),
-        marginBottom: normalize(40)
-    },
+  containerHeader: {
+    marginBottom: normalize(30),
+  },
 
-    containerHeader: {
-        marginBottom: normalize(30)
-    },
+  lblTitle: {
+    fontFamily: "kodchasan-regular",
+    fontSize: RFPercentage(2.7),
+    textAlign: "center",
+    color: theme.GlobalColorsApp.lblGrayPrimary,
+  },
 
-    lblTitle: {
-        fontFamily: 'kodchasan-regular',
-        fontSize: RFPercentage(2.7),
-        textAlign: 'center',
-        color: theme.GlobalColorsApp.lblGrayPrimary
-    },
+  containerList: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: normalize(20),
+    borderWidth: 1,
+    borderColor: theme.GlobalColorsApp.btnGray,
+    borderRadius: 10,
+    padding: normalize(10),
+    backgroundColor: "#f2f2f2",
+    marginBottom: normalize(20),
+  },
 
-    containerList: {
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center',
-        paddingHorizontal: normalize(20),
-        borderWidth: 1,
-        borderColor: theme.GlobalColorsApp.btnGray,
-        borderRadius: 10,
-        padding: normalize(10)
-    },
+  nameList: {
+    fontFamily: "kodchasan-regular",
+    flex: 3,
+  },
 
-    nameList: {
-        fontFamily: 'kodchasan-regular',
-        flex: 3
-    },
+  iconList: {
+    flex: 1,
+  },
 
-    iconList: {
-        flex: 1
-    },
+  emulateStyleCombo: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: theme.GlobalColorsApp.btnGray,
+    borderRadius: 10,
 
-    emulateStyleCombo: {
-        flex: 1,
-        borderWidth: 1,
-        borderColor: theme.GlobalColorsApp.btnGray,
-        borderRadius: 10,
+    marginTop: normalize(10),
+  },
+  headerRed: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginHorizontal: normalize(1, "height"),
+    borderWidth: 1,
+    borderColor: theme.GlobalColorsApp.lblRedPrimary,
+    alignItems: "center",
+    height: normalize(50, "width"),
+    backgroundColor: theme.GlobalColorsApp.lblRedFourth,
+  },
 
-        marginTop: normalize(10),
+  lblHeaderRed: {
+    fontFamily: "kodchasan-extraLight",
+    fontSize: RFPercentage(3.2),
+    color: theme.GlobalColorsApp.lblRedPrimary,
+    marginLeft: normalize(10, "height"),
+  },
+  item: {
+    flex: 1,
+    width: "100%",
+    flexDirection: "row",
+    borderRadius: 10,
+    alignItems: "center",
+    alignSelf: "center",
+    // justifyContent:'space-between',
+    borderWidth: 1.3,
+    marginBottom: normalize(20, "height"),
+    paddingHorizontal: normalize(15, "height"),
+    paddingVertical: normalize(15, "height"),
+  },
 
-    },
+  containerIcon: {
+    flex: 1,
+    alignSelf: "center",
+    borderRadius: 5,
+  },
+
+  containerLabels: {
+    flex: 5,
+    alignSelf: "center",
+    paddingLeft: normalize(10),
+  },
+
+  containerArrow: {
+    flex: 1,
+    alignSelf: "center",
+    alignItems: "flex-end",
+  },
+
+  lblNumQuestions: {
+    fontFamily: "kodchasan-extraLight",
+    fontSize: Platform.OS === "ios" ? RFPercentage(1.6) : RFPercentage(1.8),
+    color: "#999999",
+  },
+
+  iconMain: {
+    // padding: Platform.OS === 'ios' ? normalize(5, 'height') : normalize(10, 'height'),
+  },
+
+  lblCategoryName: {
+    fontFamily: "kodchasan-bold",
+    fontSize: Platform.OS === "ios" ? RFPercentage(2.0) : RFPercentage(2.2),
+  },
 });
