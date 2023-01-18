e975e44078176368681e4d88ff2305693434db95
victor <vr24355@gmail.com>
2023-01-18 14:08:44 -0500

    Lenguaje español footer

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -3,7 +3,7 @@ import { ActivityIndicator, View, TouchableOpacity, Alert } from "react-native";
 import { Image, Text } from "react-native-elements";
 import { storageResult, screen } from "../../../utils";
 import { styles } from "./Footer.style";
-import { useNavigation } from "@react-navigation/native";
+import { LinkingContext, useNavigation } from "@react-navigation/native";
 
 export function Footer() {
   const [language, setLanguage] = useState(false);
@@ -26,16 +26,22 @@ export function Footer() {
     await storageResult.removeItemValue("@SessionIdCountry");
     await storageResult.removeItemValue("@SessionResponsibleList");
     await storageResult.removeItemValue("@Session");
-    await storageResult.removeItemValue("@userId");
-    navigation.navigate(screen.account.tab, {
-      screen: screen.account.login,
-    });
-
+    const valorLenguage = await storageResult.getDataFormat("@SessionLanguage");
+    //console.log(DataLenguage2);
+    if (!valorLenguage) {
+      navigation.navigate(screen.account.tab, {
+        screen: screen.account.login,
+      });
+    }
+    else{
+      console.log(valorLenguage);
+    }
+    
   };
   const closeSession = () => {
     Alert.alert(
       "Alerta",
-      language == "pt" ? "¿Está seguro de que desea cerrar la sesión?" : "¿Seguro quieres salir de sesión?",
+      language == "pt" ? "Tem certeza que deseja sair?" : "¿Seguro quieres salir de sesión?",
       [
         {
           text: "Cancelar",
